6ac11c4be49b128971dece15eff24adff5d71977
jaewon <janga9710@naver.com>
2023-07-11 15:10:22 +0900

    [DOCS] : 📚

--- lib/image_AddPage.dart
+++ lib/image_AddPage.dart
@@ -1,1 +1,30 @@
-// 연결하였습니다.
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("home page"),
+      ),
+    );
+  }
+}
