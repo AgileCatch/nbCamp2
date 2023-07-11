df6c7a9beb7eb0c6616930ebc0b39fed18dce9c9
wjdehrbs <rhtmxhavus@naver.com>
2023-07-11 19:12:21 +0900

    Update detailPage.dart
    [FEAT] : ⚙️, [FIX] : 🔧

--- lib/detailPage.dart
+++ lib/detailPage.dart
@@ -15,40 +15,186 @@ class MyApp extends StatelessWidget {
 class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Image widget'),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leadingWidth: 75,
-        leading: TextButton(
-          onPressed: () {},
-          child: Text(
-            "back",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-              fontWeight: FontWeight.w600, // 리딩부분
-            ),
-          ),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              "modify",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600), // actions부분
-            ),
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
       ),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Image.asset('flutter image.png')],
+      appBar: AppBar(
+          title: Text('Image widget'),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leadingWidth: 75,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              size: 35,
+            ),
+            onPressed: () {
+              // 뒤로가기 아이콘 버튼 실행
+              print('back button is clicked');
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.edit,
+                color: Colors.black,
+                size: 35,
+              ),
+              onPressed: () {
+                print('modify button is clicked'); //수정 아이콘 버튼 실행
+              },
+            ),
+          ]),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  "https://cdn-icons-png.flaticon.com/512/188/188987.png?w=826&t=st=1689056998~exp=1689057598~hmac=7e20f8df6111b8611d0a4ef66fe147a3ab72d081e7eb4aff7a38508179afd927",
+                  width: 150,
+                  height: 200,
+                ), // 메인사진 위치
+                Text(
+                  '이   름',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  '영어  닉네임',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '생년월일',
+                      style: TextStyle(
+                          fontSize: 17, fontWeight: FontWeight.w600, height: 2),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '1997.06.02',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 380,
+                  margin: EdgeInsets.only(top: 0.2),
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 0.8,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '이메일주소',
+                      style: TextStyle(
+                          fontSize: 17, fontWeight: FontWeight.w600, height: 2),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'abcd1234@gmail.com',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 380,
+                  margin: EdgeInsets.only(top: 0.2),
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 0.8,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '사는 곳',
+                      style: TextStyle(
+                          fontSize: 17, fontWeight: FontWeight.w600, height: 2),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '한국',
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 380,
+                  margin: EdgeInsets.only(top: 0.2),
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 0.8,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'MBTI',
+                      style: TextStyle(
+                          fontSize: 17, fontWeight: FontWeight.w600, height: 2),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'ABCD',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 380,
+                  margin: EdgeInsets.only(top: 0.2),
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 0.8,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '취미',
+                      style: TextStyle(
+                          fontSize: 17, fontWeight: FontWeight.w600, height: 2),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '노래부르기',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 380,
+                  margin: EdgeInsets.only(top: 0.2),
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 0.8,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
